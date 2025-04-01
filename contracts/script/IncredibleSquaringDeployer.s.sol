8b1f86c8d780552848170f0241331cac8de96626
Ron Turetzky <turetzkyron@gmail.com>
2025-04-01 17:11:13 +0200

    chore: hardcoding mainnet seth

--- contracts/script/IncredibleSquaringDeployer.s.sol
+++ contracts/script/IncredibleSquaringDeployer.s.sol
@@ -105,10 +105,9 @@ contract IncredibleSquaringDeployer is Script {
         FundOperator.fund_operator(address(erc20Mock), isConfig.operator_addr, 15000e18);
         FundOperator.fund_operator(address(erc20Mock), isConfig.operator_2_addr, 30000e18);
         console.log(isConfig.operator_2_addr);
-        (bool s,) = isConfig.operator_2_addr.call{value: 0.1 ether}("");
-        require(s);
-        console.log(isConfig.operator_2_addr.balance);
-        incredibleSquaringStrategy = IStrategy(StrategyFactory(configData.strategyFactory).deployNewStrategy(erc20Mock));
+
+        address stETHStrategy = 0x93c4b944D05dfe6df7645A86cd2206016c51564D; 
+        incredibleSquaringStrategy = IStrategy(stETHStrategy);
         rewardscoordinator = configData.rewardsCoordinator;
 
         proxyAdmin = UpgradeableProxyLib.deployProxyAdmin();
